65f13e4a84d506298f5c2901b37e43013e958866
Kuhle104 <kuhlemvunge502@gmail.com>
2026-08-14 11:12:00 +0200

    third commit
    kids are bad as hell

--- FirstDayOfChristmas/Program.cs
+++ FirstDayOfChristmas/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            Console.WriteLine("This is a c# console application.");
+            Console.WriteLine("This is a bad day for santa");
             Console.WriteLine("Running on .NET 10.0 with C# 14.0 language features.");
 
 
