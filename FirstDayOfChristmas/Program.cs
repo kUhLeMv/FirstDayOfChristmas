8f56fb6433bf688a962d79216d8b37cee31af13f
Kuhle104 <kuhlemvunge502@gmail.com>
2026-08-14 10:35:27 +0200

    First Commit
    On the way to Christmas

---
+++ FirstDayOfChristmas/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace FirstDayOfChristmas
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("This is a c# console application.");
+            Console.WriteLine("Running on .NET 10.0 with C# 14.0 language features.");
+
+
+        }
+    }
+}
